83a87d379f70560b1abc1349c2b0741fb6527ae2
Twana Daniel <twanaazi@ie.ibm.com>
2018-04-17 11:08:38 +0100

    Update repo to use IBM Cloud Object Storage service

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,14 @@
-softlayer_username = "<ADD-Softlayer-Username-Here>"
-softlayer_api_key = "<ADD-Softlayer-API-KEY-Here>"
+# Enter your IBM Infrastructure (SoftLayer) full username, you can get this using: https://control.bluemix.net/account/user/profile
+softlayer_username = "<ADD-Softlayer-Username-here>"
+
+# Enter your IBM Infrastructure (SoftLayer) API key, you can get this using: https://control.bluemix.net/account/user/profile
+softlayer_api_key = "<ADD-Softlayer-API-KEY-here>"
+
+# Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://console.bluemix.net/iam#/apikeys
+ibmcloud_api_key = "<ADD-IBM-Cloud-Api-key>"
+
+# Enter your IBM Cloud org name, you can get your org name under your IBM Cloud dashboard account: https://console.bluemix.net/dashboard
+org_name = "<ADD-your-org-name>"
+
+# Enter your IBM Cloud space name, you can get your space name under your IBM Cloud dashboard account: https://console.bluemix.net/dashboard
+space_name = "<ADD-your-space-name>"
--- vm.tf
+++ vm.tf
@@ -13,67 +13,79 @@
 #
 ################################################################
 variable "hostname" {
-  default = "lamp"
+  default     = "lamp"
   description = "LAMP stack using IaC"
 }
+
 variable "domain" {
-  default = "IBM.cloud"
+  default     = "IBM.cloud"
   description = "The domain for the instance."
 }
+
 variable "datacenter" {
-  default = "wdc01"
+  default     = "wdc01"
   description = "The data center to create resources in."
 }
+
 variable "os_reference_code" {
-  default = "CENTOS_7"
+  default     = "CENTOS_7"
   description = "The operating system reference code used to provision the computing instance."
 }
+
 variable "cores" {
-  default = "1"
+  default     = "1"
   description = "The number of CPU cores to allocate."
 }
+
 variable "memory" {
-  default = "1024"
+  default     = "1024"
   description = "The amount of memory (in Mb) to allocate."
 }
+
 variable "disk_size" {
-  default = "25"
+  default     = "25"
   description = "The numeric disk sizes (in GB) for the instance’s block device and disk image settings."
 }
+
 variable "private_network_only" {
-  default = "false"
+  default     = "false"
   description = "When set to true, a compute instance only has access to the private network."
 }
+
 variable "network_speed" {
-  default = "100"
+  default     = "100"
   description = "The connection speed (in Mbps) for the instance’s network components."
 }
+
 variable "tags" {
-  default = "LAMP"
+  default     = "LAMP"
   description = "Descriptive tags to label the resource."
 }
 
 variable "ssh_user" {
-  default = "root"
+  default     = "root"
   description = "The default username for the VM."
 }
+
 variable "ssh_label" {
-  default = "public ssh key - VM"
+  default     = "public ssh key - VM"
   description = "An identifying label to assign to the SSH key."
 }
+
 variable "ssh_notes" {
-  default = "SSH_note"
+  default     = "SSH_note"
   description = "A description to assign to the SSH key."
 }
+
 variable "ssh_key" {
-  default = "<ADD-ssh_key-Here>"
+  default     = "<ADD-SSH-KEY-HERE>"
   description = "Your public SSH key to access the VM."
 }
 
 resource "ibm_compute_ssh_key" "ssh_key" {
-    label = "${var.ssh_label}"
-    notes = "${var.ssh_notes}"
-    public_key = "${var.ssh_key}"
+  label      = "${var.ssh_label}"
+  notes      = "${var.ssh_notes}"
+  public_key = "${var.ssh_key}"
 }
 
 resource "ibm_compute_vm_instance" "vm" {
@@ -95,5 +107,5 @@ resource "ibm_compute_vm_instance" "vm" {
 }
 
 output "public_ip" {
-	value = "http://${ibm_compute_vm_instance.vm.ipv4_address}"
+  value = "http://${ibm_compute_vm_instance.vm.ipv4_address}"
 }
